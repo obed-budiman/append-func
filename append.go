b43dd95bae620fe03f9199d72091b728e5518974
obedbudiman <obed.budiman@tiket.com>
2022-08-31 15:13:52 +0700

    create insert function

--- append.go
+++ append.go
@@ -10,3 +10,17 @@ func NewAppend() *Append {
 		NextIndex: 0,
 	}
 }
+
+func (a *Append) Insert(newData int) {
+	fmt.Printf("--inserting new data : %v\n", newData)
+
+	if a.NextIndex > cap(a.Data)-1 {
+		newCapacity := cap(a.Data) * 2
+		newArray := make([]int, newCapacity)
+		copy(newArray, a.Data)
+		a.Data = newArray
+	}
+
+	a.Data[a.NextIndex] = newData
+	a.NextIndex++
+}
